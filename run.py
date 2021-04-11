c20fcd681370692677bf3af3e98f3216b3fdc77f
Zhiyao Ma <221thunder@gmail.com>
2021-04-11 11:29:07 -0400

    Add reverse training.

--- run.py
+++ run.py
@@ -183,7 +183,7 @@ def TrainQlearning(model: nn.Module, env: PathPlanningEnv, config: settings.Conf
 
 def TrainQlearningMultiple(model: nn.Module, envs: list, config: settings.Config, loss: torch.nn.modules.loss = nn.MSELoss(), reset_model: bool = True):
     '''
-    This function trains the model using the Q-learning algorithm
+    This function trains the model using the Q-learning algorithm on multiple environments
     '''
     device = config.device
     if "cuda" in device:
@@ -260,9 +260,157 @@ def TrainQlearningMultiple(model: nn.Module, envs: list, config: settings.Config
             tot_reward = torch.Tensor([tot_reward])
             tot_reward = tot_reward.view(1, *tot_reward.shape)
             loss = loss_func(preds[choice], tot_reward)
+
+            l1_reg = torch.tensor(0., requires_grad=True)
+            for name, param in model.named_parameters():
+                if 'weight' in name and 'bn' not in name:
+                    l1_reg = l1_reg + torch.norm(param, 1)
+            loss += l1_reg * 0.001
+
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
+
+    return rewards, losses
+
+
+def ReverseAction(action: int):
+    parity = action % 2
+    change = parity * (-2) + 1
+    return action + change
+
+
+def TrainQlearningMultipleReverse(model: nn.Module, envs: list, config: settings.Config, loss: torch.nn.modules.loss = nn.MSELoss(), reset_model: bool = True):
+    '''
+    This function trains the model using the Q-learning algorithm on multiple environments with reverse updating
+    '''
+    device = config.device
+    if "cuda" in device:
+        model.to(device)
+
+    epsilon, lr = config.epsilon, config.learning_rate
+    num_of_play, max_play_length = config.epochs, config.max_play_length
+    gamma, epsilon, epsilon_low, epsilon_step = config.gamma, config.epsilon, config.epsilon_low, config.epsilon_step
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    loss_func = loss
+
+    if reset_model:
+        init_seed()
+        model.apply(init_weights)
+
+    losses = []
+
+    for i in range(1, num_of_play+1):
+        env = random.choice(envs)
+        env.random_reset()
+        if i % 100 == 0:
+            print("play round: {}, ave reward (last {}): {:.4f}".format(
+                i, 100, sum(rewards[-100:])/100))
+            if epsilon > epsilon_low:
+                epsilon -= epsilon_step
+
+        model.eval()
+        done = False
+        counter = 0
+        moves = []
+        imm_rewards = []
+        while counter <= max_play_length and not done:
+            preds = []
+            state = env.grid.clone().detach()
+            state = state.view(1, *state.shape)
+            for action in env.actions:
+                action = action.view(1, *action.shape)
+                pred = model(state, action)
+                preds.append(pred)
+
+            p = np.random.uniform(0, 1)
+            if p < epsilon:
+                choice = np.random.randint(0, 4)
+            else:
+                list_pred = [x.item() for x in preds]
+                max_pred = np.amax(list_pred)
+                max_positions = np.argwhere(
+                    list_pred == max_pred).flatten().tolist()
+                choice = random.choice(max_positions)
+            _, imm_reward, done, _ = env.step(choice, early_stop=False, q_learning=True)
+            moves.append(choice)
+            imm_rewards.append(imm_reward)
+            
+            counter += 1
+        
+        rewards = imm_rewards.copy()
+        
+        if done:
+            counter = 0
+            done = False
+            model.train()
+            
+            cur_row, cur_col = env.goal_row, env.goal_col
+            cur_action, cur_reward = moves.pop(), imm_rewards.pop()
+            rev_action = ReverseAction(cur_action)
+            env.step(rev_action, early_stop=False, q_learning=True)
+
+            state = env.grid.clone().detach()
+            state = state.view(1, *state.shape)
+
+            action_vec = env.actions[cur_action]
+            action_vec = action_vec.view(1, *action_vec.shape)
+            optimizer.zero_grad()
+            pred_reward = model(state, action_vec)
+
+            real_reward = torch.Tensor([cur_reward])
+            real_reward = real_reward.view(1, *real_reward.shape)
+            loss = loss_func(pred_reward, real_reward)
+
+            losses.append(loss)
+
+            loss.backward()
+            optimizer.step()
+
+            with torch.no_grad():
+                next_preds = []
+                state = env.grid.clone().detach()
+                state = state.view(1, *state.shape)
+                for action in env.actions:
+                    action = action.view(1, *action.shape)
+                    next_pred = model(state, action)
+                    next_preds.append(next_pred.item())
+                future_reward = max(next_preds)
+            
+            while len(moves) > 0 and len(imm_rewards) > 0:
+                cur_row, cur_col = env.goal_row, env.goal_col
+                cur_action, cur_reward = moves.pop(), imm_rewards.pop()
+                rev_action = ReverseAction(cur_action)
+                if cur_reward == 0:
+                    env.step(rev_action, early_stop=False, q_learning=True)
+
+                state = env.grid.clone().detach()
+                state = state.view(1, *state.shape)
+
+                action_vec = env.actions[cur_action]
+                action_vec = action_vec.view(1, *action_vec.shape)
+                optimizer.zero_grad()
+                pred_reward = model(state, action_vec)
+
+                real_reward = cur_reward + gamma * future_reward
+                real_reward = torch.Tensor([real_reward])
+                real_reward = real_reward.view(1, *real_reward.shape)
+                loss = loss_func(pred_reward, real_reward)
+                
+                losses.append(loss)
+
+                loss.backward()
+                optimizer.step()
+                
+                with torch.no_grad():
+                    next_preds = []
+                    state = env.grid.clone().detach()
+                    state = state.view(1, *state.shape)
+                    for action in env.actions:
+                        action = action.view(1, *action.shape)
+                        next_pred = model(state, action)
+                        next_preds.append(next_pred.item())
+                    future_reward = max(next_preds)
 
     return rewards, losses
 
