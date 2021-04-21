64b4d73b80f5859982b688734116ccef05aa5c1b
Yang-Yihang <yihangyang1993@gmail.com>
2021-04-21 18:08:06 -0400

    more implementation

--- multi-agent/MultipleActorEnv.py
+++ multi-agent/MultipleActorEnv.py
@@ -1,55 +1,125 @@
 import random
 import torch
-from PathPlanningEnv import PathPlanningEnv
+import numpy as np
 
-class MultipleActorEnv():
-    def __init__(self, *, env_num, height, width, obs_count, random_seed=None):
-        assert env_num >= 1, "Cannot create less than 1 Envs"
-        assert height > 0, "illegal height"
-        assert width > 0, "illegal width"
-        assert height * width > obs_count + 2, "illegal height, width, and obs_count combination"
 
+class MultiActorEnv():
+    def __init__(self, *,
+                 agent_count,
+                 height, width, obs_count, grid=None,
+                 sources=None, goals=None,
+                 random_seed=None):
+
+        self.agent_count = agent_count
+        self.height = height
+        self.width = width
+        self.obs_count = obs_count
+
+        self.sources = list()
+        self.targets = list()
         if random_seed is None:
             random_seed = 100
-        self.env_num = env_num
-        self.envs = []
+        
+        self._init_random_grid(self.agent_count, self.height, self.width, self.obs_count, random_seed)
+
+        self.actions = [
+            torch.Tensor().new_tensor([1, 0, 0, 0, 0], dtype=torch.float32, requires_grad=False),  # up
+            torch.Tensor().new_tensor([0, 1, 0, 0, 0], dtype=torch.float32, requires_grad=False),  # down
+            torch.Tensor().new_tensor([0, 0, 1, 0, 0], dtype=torch.float32, requires_grad=False),  # left
+            torch.Tensor().new_tensor([0, 0, 0, 1, 0], dtype=torch.float32, requires_grad=False),  # right
+            torch.Tensor().new_tensor([0, 0, 0, 0, 1], dtype=torch.float32, requires_grad=False)   # stay still
+        ]
+
+    def _init_random_grid(self, agent_count: int, height: int, width: int, obs_count: int, random_seed: int):
+        # check total number of grid points
+        total_size = height*width
+        assert total_size >= 2, 'Error: expect height * width >= 2'
+
+        # initialize the grid
+        self.grid_initial = torch.zeros(size=(3, height, width), requires_grad=False)
+
+        # randomly select part of grid points as obstacles, and agent_count (source, target) pair
         random.seed(random_seed)
-        for _ in range(self.env_num):
-            model_settings = {
-                'height' : height,
-                'width' : width,
-                'obs_count' : obs_count,
-                'random_seed' : random.randint(0, 2**16)}
-            # print(model_settings)
-            self.envs.append(PathPlanningEnv(**model_settings))
-        self.actions = self.envs[0].actions
-    
-    def display(self):
-        for i in range(self.env_num):
-            print("{}-th env".format(i))
-            self.envs[i].display()
+        random_selections = random.sample(range(total_size), k=min(obs_count+2*agent_count, total_size))
+
+        for i in range(agent_count):
+            num = random_selections.pop()
+            source = [num//width, num % width]
+            self.sources.append(source)
+            self.grid_initial[0, source[0], source[1]] = 1
+
+            num = random_selections.pop()
+            target = [num//width, num % width]
+            self.targets.append(target)
+            self.grid_initial[1, target[0], target[1]] = 1
+
+        for i in random_selections:
+            row = i // width
+            col = i % width
+            self.grid_initial[2, row, col] = 1
+
+        self.reset()
+
+    def _init_from_grid(self, obs_grid, p_init_row, p_init_col, goal_row, goal_col):
+        self.height = obs_grid.shape[0]
+        self.width = obs_grid.shape[1]
+        self.grid_initial = torch.zeros(
+            size=(3, self.height, self.width), requires_grad=False)
+        for i in range(self.height):
+            for j in range(self.width):
+                self.grid_initial[2, i, j] = obs_grid[i, j]
+
+        self.init_row, self.init_col = p_init_row, p_init_col
+        self.grid_initial[0, self.init_row, self.init_col] = 1
+
+        self.goal_row = goal_row
+        self.goal_col = goal_col
+        self.grid_initial[1, self.goal_row, self.goal_col] = 1
+
+        self.reset()
 
     def reset(self):
-        for env in self.envs:
-            env.reset()
-    
-    def observation(self):
-        batch_done = [env.done for env in self.envs]
-        batch_states = [env.grid for env in self.envs if not env.done]
-        batch_states = torch.stack(batch_states)
-        print(type(batch_states), batch_states.shape)
-        return batch_done, batch_states
+        self.grid = self.grid_initial.detach().clone()
+        self.done = False
 
-    def step(self, mask, actions, early_stop=True, q_learning=False):
-        assert len(mask)==self.env_num, "dimension of mask does not match env_num"
-        assert len(actions)==self.env_num, "dimension of actions does not match env_num"
-        
-        rewards = [0]*self.env_num
-        dones = [True]*self.env_num
-        for i in range(self.env_num):
-            if (mask[i]==1): 
-                _, reward, done, _ = self.envs[i].step(actions[i])
-                rewards[i] = reward
-                dones[i] = done
+    def random_reset(self):
+        assert self.grid_initial.shape[0] != 0, 'Error: expect an initial grid'
+        assert self.init_row is not None and self.init_col is not None, 'Error: expect a initial position'
 
-        return rewards, dones
+        initials = [(0, 0), (0, self.width-1), (self.height-1, 0),
+                    (self.height-1, self.width-1)]
+
+        self.grid = self.grid_initial.detach().clone()
+        self.cur_row, self.cur_col = random.choice(initials)
+        self.done = False
+        self.trace = [(self.cur_row, self.cur_row)]
+
+    def display(self, grid=None):
+        if grid is None:
+            grid = self.grid
+        assert grid.shape[0] != 0, 'Error: expect a non-empty grid'
+
+        displ_board = [[' ' for _ in range(self.width)]
+                       for _ in range(self.height)]
+        for i in range(self.height):
+            for j in range(self.width):
+                if grid[0, i, j] == 1:
+                    displ_board[i][j] = 'P'
+                elif grid[1, i, j] == 1:
+                    displ_board[i][j] = 'T'
+                elif grid[2, i, j] == 1:
+                    displ_board[i][j] = 'O'
+        for row in displ_board:
+            print(row)
+
+    def step(self, action, early_stop=True, q_learning=False):
+        return 1
+
+
+def main():
+    env = MultiActorEnv(agent_count=2, height=5, width=10, obs_count=5, random_seed=100)
+    print(env.grid)
+    env.display()
+
+if __name__ == "__main__":
+    main()
