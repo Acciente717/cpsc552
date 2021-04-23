4f50ef344aa012c12bd7abdf30c36ef0ee4361b8
Yang-Yihang <yihangyang1993@gmail.com>
2021-04-22 22:54:40 -0400

    bug fix

--- multi-agent/MultipleActorEnv.py
+++ multi-agent/MultipleActorEnv.py
@@ -2,12 +2,19 @@ import random
 import torch
 import numpy as np
 
+class Loc():
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+    def copy(self):
+        return Loc(self.row, self.col)
 
 class MultiActorEnv():
     def __init__(self, *,
                  agent_count,
                  height, width, obs_count, grid=None,
-                 sources=None, goals=None,
+                 srcs=None, tgts=None,
                  random_seed=None):
 
         self.agent_count = agent_count
@@ -15,8 +22,9 @@ class MultiActorEnv():
         self.width = width
         self.obs_count = obs_count
 
-        self.sources = list()
-        self.targets = list()
+        self.srcs = list()
+        self.tgts = list()
+        self.locs = list()
         if random_seed is None:
             random_seed = 100
         
@@ -44,80 +52,94 @@ class MultiActorEnv():
 
         for i in range(agent_count):
             num = random_selections.pop()
-            source = [num//width, num % width]
-            self.sources.append(source)
-            self.grid_initial[0, source[0], source[1]] = 1
+            source = Loc(num//width, num % width)
+            self.srcs.append(source)
+            self.grid_initial[0, source.row, source.col] = i + 1
 
             num = random_selections.pop()
-            target = [num//width, num % width]
-            self.targets.append(target)
-            self.grid_initial[1, target[0], target[1]] = 1
+            target = Loc(num//width, num % width)
+            self.tgts.append(target)
+            self.grid_initial[1, target.row, target.col] = i + 1
 
-        for i in random_selections:
-            row = i // width
-            col = i % width
+        for num in random_selections:
+            row = num // width
+            col = num % width
             self.grid_initial[2, row, col] = 1
-
-        self.reset()
-
-    def _init_from_grid(self, obs_grid, p_init_row, p_init_col, goal_row, goal_col):
-        self.height = obs_grid.shape[0]
-        self.width = obs_grid.shape[1]
-        self.grid_initial = torch.zeros(
-            size=(3, self.height, self.width), requires_grad=False)
-        for i in range(self.height):
-            for j in range(self.width):
-                self.grid_initial[2, i, j] = obs_grid[i, j]
-
-        self.init_row, self.init_col = p_init_row, p_init_col
-        self.grid_initial[0, self.init_row, self.init_col] = 1
-
-        self.goal_row = goal_row
-        self.goal_col = goal_col
-        self.grid_initial[1, self.goal_row, self.goal_col] = 1
 
         self.reset()
 
     def reset(self):
         self.grid = self.grid_initial.detach().clone()
+        self.locs = self.srcs.copy()
         self.done = False
-
-    def random_reset(self):
-        assert self.grid_initial.shape[0] != 0, 'Error: expect an initial grid'
-        assert self.init_row is not None and self.init_col is not None, 'Error: expect a initial position'
-
-        initials = [(0, 0), (0, self.width-1), (self.height-1, 0),
-                    (self.height-1, self.width-1)]
-
-        self.grid = self.grid_initial.detach().clone()
-        self.cur_row, self.cur_col = random.choice(initials)
-        self.done = False
-        self.trace = [(self.cur_row, self.cur_row)]
 
     def display(self, grid=None):
         if grid is None:
             grid = self.grid
         assert grid.shape[0] != 0, 'Error: expect a non-empty grid'
 
-        displ_board = [[' ' for _ in range(self.width)]
+        displ_board = [['  ' for _ in range(self.width)]
                        for _ in range(self.height)]
         for i in range(self.height):
             for j in range(self.width):
-                if grid[0, i, j] == 1:
-                    displ_board[i][j] = 'P'
-                elif grid[1, i, j] == 1:
-                    displ_board[i][j] = 'T'
+                if grid[0, i, j] != 0:
+                    displ_board[i][j] = 'P' + str(int(grid[0, i, j].item()))
+                elif grid[1, i, j] != 0:
+                    displ_board[i][j] = 'T' + str(int(grid[1, i, j].item()))
                 elif grid[2, i, j] == 1:
-                    displ_board[i][j] = 'O'
+                    displ_board[i][j] = 'OO'
         for row in displ_board:
             print(row)
 
-    def step(self, action, early_stop=True, q_learning=False):
-        return 1
+    def step(self, actions: list):
+        assert len(actions) == self.agent_count, "Error: #actions is incorrect"
+        rewards = [0]*self.agent_count
+        new_locs = []
+        for i in range(self.agent_count):
+            action = actions[i]
+            new_loc = self.locs[i].copy()
+
+            if action == 'u' or action == 0:
+                new_loc.row -= 1
+            elif action == 'd' or action == 1:
+                new_loc.row += 1
+            elif action == 'l' or action == 2:
+                new_loc.col -= 1
+            elif action == 'r' or action == 3:
+                new_loc.col += 1
+            elif action == 's' or action == 4:
+                pass
+            else:
+                raise RuntimeError("Error: unknown move")
+            new_locs.append(new_loc)
+
+        for i in range(self.agent_count):
+            self.grid[0, self.locs[i].row, self.locs[i].col] = 0
+        for i in range(self.agent_count):
+            self.grid[0, new_locs[i].row, new_locs[i].col] = i + 1 
+            self.locs[i] = new_locs[i]
+
+        observation = self.grid
+
+        info = ""
+
+        return observation, rewards, self.done, info
 
 
 def main():
     env = MultiActorEnv(agent_count=2, height=5, width=10, obs_count=5, random_seed=100)
+    print(env.grid)
+    env.display()
+    print("-------------------------------------------------------------")
+    env.step(['l', 'l'])
+    print(env.grid)
+    env.display()
+    print("-------------------------------------------------------------")
+    env.step(['u', 'd'])
+    print(env.grid)
+    env.display()
+    print("-------------------------------------------------------------")
+    env.step(['u', 'd'])
     print(env.grid)
     env.display()
 
