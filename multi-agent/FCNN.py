2d6019cdedc86c428d47776363c4f30af6f775c0
Yihang Yang <yihangyang1993@gmail.com>
2021-05-17 23:26:20 -0400

    multi-agent environment

---
+++ multi-agent/FCNN.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn  # neural network modules
+import torch.nn.functional as F  # activation functions
+import torch.optim as optim  # optimizer
+from torch.autograd import Variable # add gradients to tensors
+from torch.nn import Parameter # model parameter functionality
+
+class FCNN(nn.Module):
+    def __init__(self, input_dim=154, output_dim=1, n_hidden_1=100, n_hidden_2=100, n_hidden_3=100):
+        super(FCNN, self).__init__()
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.layer1 = nn.Linear(input_dim, n_hidden_1)
+        self.layer2 = nn.Linear(n_hidden_1, n_hidden_2)
+        self.layer3 = nn.Linear(n_hidden_2, n_hidden_3)
+        self.layer4 = nn.Linear(n_hidden_3, output_dim)
+
+        self.nonlin1 = nn.ReLU()
+        self.nonlin2 = nn.ReLU()
+        self.nonlin3 = nn.ReLU()
+
+    def forward(self, states, action):
+        combined = torch.cat([states, action])
+        h1 = self.nonlin1(self.layer1(combined))
+        h2 = self.nonlin2(self.layer2(h1))
+        h3 = self.nonlin3(self.layer3(h2))
+        output = self.layer4(h2)
+        return output
--- multi-agent/MultiActorEnv.py
+++ multi-agent/MultiActorEnv.py
@@ -10,32 +10,46 @@ class Loc():
     def copy(self):
         return Loc(self.row, self.col)
 
-    def copy_from(self, loc):
+    def set_from(self, loc):
         self.row = loc.row
         self.col = loc.col
 
     def __eq__(self, other):
         return (self.row == other.row) and (self.col == other.col)
 
+    def __str__(self):
+        return "(" + str(self.row) + "," + str(self.col) + ")"
+
+    def __repr__(self):
+        return str(self)
+
 class MultiActorEnv():
     def __init__(self, *,
-                 agent_count,
+                 actor_number,
                  height, width, obs_count, grid=None,
                  srcs=None, tgts=None,
                  random_seed=None):
 
-        self.agent_count = agent_count
+        self.actor_number = actor_number
         self.height = height
         self.width = width
         self.obs_count = obs_count
 
         self.srcs = list()
         self.tgts = list()
+        self.srcs_init = list()
+        self.tgts_init = list()
         self.locs = list()
+        self.idx = list()
+
+        self.tgt_dones = list()
+        self.idx_dones = list()
+
+
         if random_seed is None:
             random_seed = 100
         
-        self._init_random_grid(self.agent_count, self.height, self.width, self.obs_count, random_seed)
+        self._init_random_grid(self.actor_number, self.height, self.width, self.obs_count, random_seed)
 
         self.actions = [
             torch.Tensor().new_tensor([1, 0, 0, 0, 0], dtype=torch.float32, requires_grad=False),  # up
@@ -48,8 +62,9 @@ class MultiActorEnv():
         self.out_bound_penalty = -0.5
         self.obstacle_penalty = -0.5
         self.collision_penalty = -0.5
+        self.stationary_penalty = -0.01
 
-    def _init_random_grid(self, agent_count: int, height: int, width: int, obs_count: int, random_seed: int):
+    def _init_random_grid(self, actor_number: int, height: int, width: int, obs_count: int, random_seed: int):
         # check total number of grid points
         total_size = height*width
         assert total_size >= 2, 'Error: expect height * width >= 2'
@@ -57,31 +72,40 @@ class MultiActorEnv():
         # initialize the grid
         self.grid_initial = torch.zeros(size=(3, height, width), requires_grad=False)
 
-        # randomly select part of grid points as obstacles, and agent_count (source, target) pair
+        # randomly select part of grid points as obstacles, and actor_number (source, target) pair
         random.seed(random_seed)
-        random_selections = random.sample(range(total_size), k=min(obs_count+2*agent_count, total_size))
+        random_selections = random.sample(range(total_size), k=min(obs_count+2*actor_number, total_size))
 
-        for i in range(agent_count):
+        for i in range(actor_number):
             num = random_selections.pop()
             source = Loc(num//width, num % width)
-            self.srcs.append(source)
+            self.srcs_init.append(source)
             self.grid_initial[0, source.row, source.col] = i + 1
 
             num = random_selections.pop()
             target = Loc(num//width, num % width)
-            self.tgts.append(target)
+            self.tgts_init.append(target)
             self.grid_initial[1, target.row, target.col] = i + 1
 
+        self.obs_list = [[],[],[]]
         for num in random_selections:
             row = num // width
             col = num % width
             self.grid_initial[2, row, col] = 1
+            self.obs_list[0].append(col)
+            self.obs_list[1].append(row)
 
         self.reset()
 
     def reset(self):
+        self.srcs = [x.copy() for x in self.srcs_init]
+        self.tgts = [x.copy() for x in self.tgts_init]
+        self.actor_number = len(self.srcs_init)
         self.grid = self.grid_initial.detach().clone()
-        self.locs = self.srcs.copy()
+        self.locs = [x.copy() for x in self.srcs]
+        self.idx = list(range(1, self.actor_number+1))
+        self.tgt_dones.clear()
+        self.idx_dones.clear()
 
     def display(self, grid=None):
         if grid is None:
@@ -100,6 +124,33 @@ class MultiActorEnv():
                     displ_board[i][j] = 'OO'
         for row in displ_board:
             print(row)
+
+    def get_obs(self):
+        return self.obs_list
+
+    def get_tgts(self):
+        tgts_list = [[],[],[]]
+        for i in range(self.actor_number):
+            tgts_list[0].append(self.tgts[i].col)
+            tgts_list[1].append(self.tgts[i].row)
+            tgts_list[2].append(self.idx[i])
+        for i in range(len(self.idx_dones)):
+            tgts_list[0].append(self.tgt_dones[i].col)
+            tgts_list[1].append(self.tgt_dones[i].row)
+            tgts_list[2].append(self.idx_dones[i])
+        return tgts_list
+
+    def get_locs(self):
+        locs_list = [[],[],[]]
+        for i in range(self.actor_number):
+            locs_list[0].append(self.locs[i].col)
+            locs_list[1].append(self.locs[i].row)
+            locs_list[2].append(self.idx[i])
+        for i in range(len(self.idx_dones)):
+            locs_list[0].append(self.tgt_dones[i].col)
+            locs_list[1].append(self.tgt_dones[i].row)
+            locs_list[2].append(self.idx_dones[i])
+        return locs_list
     
     def __out_of_boundary(self, loc: Loc):
         '''
@@ -125,32 +176,65 @@ class MultiActorEnv():
         '''
         return (self.grid[0, loc.row, loc.col] != 0) and (self.grid[0, loc.row, loc.col] != agent_index)
 
+    def get_actor_number(self):
+        return self.actor_number
+
+    def get_state(self, actor_index: int):
+        '''
+        Return [4*height*width] tensor
+        4*height*width comes from:
+            0th layer: current agent location
+            1st layer: agent target location
+            2nd layer: obstacles
+            3rd layer: other agents
+        '''
+        assert actor_index < self.actor_number, "Invalid actor index"
+        state = torch.zeros(size=(4, self.height, self.width))
+        state[0, self.locs[actor_index].row, self.locs[actor_index].col] = 1
+        state[1, self.tgts[actor_index].row, self.tgts[actor_index].col] = 1
+        state[2] = self.grid[2].detach().clone()
+        for i in range(len(self.locs)):
+            if i != actor_index:
+                state[3, self.locs[i].row, self.locs[i].col] = 1
+        return state.view(4*self.height*self.width)
+
+    def remove_dones(self):
+        hi_index = self.actor_number-1
+        for i in range(hi_index, -1, -1):
+            if (self.locs[i] == self.tgts[i]):
+                self.grid[0, self.tgts[i].row, self.tgts[i].col] = 0
+                self.grid[1, self.tgts[i].row, self.tgts[i].col] = 0
+                self.grid[2, self.tgts[i].row, self.tgts[i].col] = 1
+
+                self.idx_dones.append(self.idx[i])
+                self.tgt_dones.append(self.tgts[i].copy())
+                self.actor_number -= 1
+
+                self.srcs.pop(i)
+                self.tgts.pop(i)
+                self.locs.pop(i)
+                self.idx.pop(i)
+
     def step(self, actions: list):
         '''
         Take a step based on given actions.
         Returns
-            observations: a list of [3, height, weight] tensors
-                0th layer: current agent location
-                1st layer: current agent location
-                2nd layer: obstacles and other agents
-            rewards: a list of reward, an agent can move to a new location only when
+            rewards: [batch_size, 1] tensor, an agent can move to a new location only when
                 1. new location is not out of bouondary, otherwise, return out_bound_penalty
                 2. new location is not a obstacle, otherwise, return obstacle_penalty
-                3. new location is currently not occupied by another agent, otherwise, return collision_penalty
+                3. new location will not be occupied by another agent, otherwise, return collision_penalty
             dones: a list of boolean values indicate whether agents have reached their targers
         Miscellaneous
             if an agent has been on its target lcoation before taking the action, then this agent will be ignored
         '''
-        assert len(actions) == self.agent_count, "Error: #actions is incorrect"
-        rewards = [0]*self.agent_count
-        dones = [False]*self.agent_count
+        rewards = [0]*len(actions)
+        dones = [False]*len(actions)
 
         # for each agent, find its new location based on its current location and the action
         new_locs = []
-        for i in range(self.agent_count):
+        for i in range(self.actor_number):
             action = actions[i]
             new_loc = self.locs[i].copy()
-
             if action == 'u' or action == 0:
                 new_loc.row -= 1
             elif action == 'd' or action == 1:
@@ -163,39 +247,48 @@ class MultiActorEnv():
                 pass
             else:
                 raise RuntimeError("Error: unknown move")
+                
             new_locs.append(new_loc)
 
-        for i in range(self.agent_count):
+        for i in range(self.actor_number):
             if self.__out_of_boundary(new_locs[i]): # if out of boundary, then do not move this agent, and give a penalty
-                new_locs[i].copy_from(self.locs[i])
+                new_locs[i].set_from(self.locs[i])
                 rewards[i] = self.out_bound_penalty
             elif self.__is_obstacle(new_locs[i]): # if this location is an obstacle, then do not move this agent, and give a penalty
-                new_locs[i].copy_from(self.locs[i])
+                new_locs[i].set_from(self.locs[i])
                 rewards[i] = self.obstacle_penalty
-            elif self.__is_occupied(new_locs[i], i+1): # if this location is occupised, then do not move this agent, and give a penalty
-                new_locs[i].copy_from(self.locs[i])
-                rewards[i] = self.obstacle_penalty
+            elif (new_locs[i] == self.tgts[i]): # if reach target, then give a reward
+                rewards[i] = self.target_reward
+            elif (new_locs[i] == self.locs[i]): # if this agent is stationary, then give a small penalty
+                rewards[i] = self.stationary_penalty
             else:
-                if (new_locs[i] == self.tgts[i]):
-                    rewards[i] = self.target_reward
+                is_collsion = False
+                for j in range(self.actor_number):
+                    if i==j: continue
+                    if (new_locs[i]==new_locs[j]):
+                        is_collsion = True
+                        break
+                if is_collsion: # if collide with another agent, then do not move this agent, and give a penalty
+                    new_locs[i].set_from(self.locs[i])
+                    rewards[i] = self.collision_penalty
         
         # clean old locations on the gird
-        for i in range(self.agent_count):
+        for i in range(self.actor_number):
             self.grid[0, self.locs[i].row, self.locs[i].col] = 0
         # add new locations to the grid
-        for i in range(self.agent_count):
-            self.grid[0, new_locs[i].row, new_locs[i].col] = i + 1 
-            self.locs[i].copy_from(new_locs[i])
+        for i in range(self.actor_number):
+            self.grid[0, new_locs[i].row, new_locs[i].col] = self.idx[i]
+            self.locs[i].set_from(new_locs[i])
 
-        observations = self.grid
+        # check done status
+        for i in range(self.actor_number):
+            dones[i] = (self.locs[i] == self.tgts[i])
 
-        info = ""
-
-        return observations, rewards, dones, info
+        return rewards, dones
 
 
 def main():
-    env = MultiActorEnv(agent_count=2, height=5, width=10, obs_count=5, random_seed=100)
+    env = MultiActorEnv(actor_number=2, height=5, width=10, obs_count=5, random_seed=100)
     print(env.grid)
     env.display()
     print("-------------------------------------------------------------")
